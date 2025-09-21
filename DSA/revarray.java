3f50e9c651a027959db8b6d0332a3d3ea7fc4df1
Ritesh-sharma-14 <r.sharma1402004@gmail.com>
2025-09-21 23:15:41 +0530

    Update revarray.java

--- DSA/revarray.java
+++ DSA/revarray.java
@@ -1,10 +1,12 @@
 package DSA;
 
 public class revarray {
+    public static void main(String[] args){
     String str="Ritesh";
     String rev=" ";
     for(int i=str.length()-1;i>=0;i--){
         rev+=str.charAt(i);
     }
     System.out.println("reversed string is: "+rev);
+    }
 }
