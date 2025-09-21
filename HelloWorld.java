2bd4604530fba2a929320b687a35abe484dd7616
Ritesh-sharma-14 <r.sharma1402004@gmail.com>
2025-09-21 22:28:27 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int n=5;
+
+        if(n%2==0){
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
     }
 }
