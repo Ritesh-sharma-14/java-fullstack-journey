bfa0930a6a8284ed478affadeeffd826e49bf4e6
Ritesh-sharma-14 <r.sharma1402004@gmail.com>
2025-09-21 22:37:52 +0530

    Create typecasting.java

---
+++ typecasting.java
@@ -0,0 +1,8 @@
+public class typecasting{
+    public static void main(String args[]){
+        int a=10;
+        double b=a; //widening(auto)
+        double x=9.8;
+        int y=(int)x; //narrowing(manual)
+    }
+}
